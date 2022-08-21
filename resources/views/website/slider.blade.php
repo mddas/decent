00e60df94f11d04ad90bd7a4e23a939d82d4f805
md <mddasgudiya@gmail.com>
2022-08-21 18:09:39 +0545

    job error fix

--- resources/views/website/slider.blade.php
+++ resources/views/website/slider.blade.php
@@ -38,7 +38,7 @@
 		    <!-- End Brochure section-->
 
 		    <!-- Job Highlight section-->
-			@if(isset($jobs))
+			@if($jobs->count()>0)
 		    <div class="job-highlight">
 		        <div class="job-detail">
 		        	<div class="owl-carousel loop job-carousel">
