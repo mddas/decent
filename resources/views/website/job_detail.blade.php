4e525e58bd48f2f0ab1bdac728b359a59ced0e4a
md <mddasgudiya@gmail.com>
2022-08-30 14:54:48 +0545

    job detail

--- resources/views/website/job_detail.blade.php
+++ resources/views/website/job_detail.blade.php
@@ -10,12 +10,12 @@
 					<div class="col-md-12 res-m-bttm-sm">
 						<div class="text">
 							<h4>Helper Urgently Needed</h4><a href="{{route('jobapply',$job->navigation->nav_name)}}" class="btn btn-outline apply-button">Apply Now</a>
-                            <div class="row">
-                            <div class="col-md-8">
-							<p>{{$job->navigation->caption}}</p>
-                            </div>
+                            <div class="row">                            
                             <div class="col-md-4">
                                 <figure><img src="{{$job->navigation->banner_image}}" alt="job-titel" class="img-fluid"></figure>
+                            </div>
+							<div class="col-md-8">
+							<p>{{$job->navigation->long_content}}</p>
                             </div>
                             </div>
 							<ul>
@@ -25,7 +25,6 @@
 								<li>Salary : {{$job->salary}}</li>
 								<li>Contract : {{$job->time_contract}} Year</li>
 							</ul>
-							<p>{{$job->navigation->long_content}}</p>
 						</div>
 					</div>
 				</div>
